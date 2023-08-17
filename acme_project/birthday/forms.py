d9aa921796cef6d7bca4cc46f53f5fe8ec522869
Denis Hrushev <super.hrushev2@mail.ru>
2023-08-17 15:15:39 +0300

    Lesson 2.5

---
+++ acme_project/birthday/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+from .models import Birthday
+
+
+class BirthdayForm(forms.ModelForm):
+
+    class Meta:
+        model = Birthday
+        fields = ('first_name', 'last_name', 'birthday')
+        widgets = {
+            'birthday': forms.DateInput(attrs={'type': 'date'})
+        }
